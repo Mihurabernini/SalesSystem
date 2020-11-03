f6b72ca5612e6a664f0b55531116bff24a3cc690
LVMD <lvmd85@gmail.com>
2020-11-03 17:15:22 +0100

    update user data

--- SalesSystem/Areas/Users/Pages/Account/Register.cshtml.cs
+++ SalesSystem/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -29,15 +27,12 @@ namespace SalesSystem.Areas.Users.Pages.Account
         private Uploadimage _uploadimage;
         private static InputModelRegister _dataUser1, _dataUser2;
         private IWebHostEnvironment _environment;
-
-
         public RegisterModel(
-            UserManager<IdentityUser> userManager, 
-            SignInManager<IdentityUser> signInManager, 
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
             RoleManager<IdentityRole> roleManager,
             ApplicationDbContext context,
-            IWebHostEnvironment environment
-            )
+            IWebHostEnvironment environment)
         {
             _context = context;
             _userManager = userManager;
@@ -47,14 +42,44 @@ namespace SalesSystem.Areas.Users.Pages.Account
             _usersRole = new LUsersRoles();
             _uploadimage = new Uploadimage();
         }
-        public void OnGet()
+        public void OnGet(int id)
         {
-            if (_dataInput != null)
+            if (id.Equals(0))
             {
-                Input = _dataInput;
-                Input.rolesLista = _usersRole.getRoles(_roleManager);
-                Input.AvatarImage = null;
-            } 
+                _dataUser2 = null;
+            }
+            if (_dataInput != null || _dataUser1 != null || _dataUser2 != null)
+            {
+                if (_dataInput != null)
+                {
+                    Input = _dataInput;
+                    Input.rolesLista = _usersRole.getRoles(_roleManager);
+                    Input.AvatarImage = null;
+                }
+                else
+                {
+                    if (_dataUser1 != null || _dataUser2 != null)
+                    {
+                        if (_dataUser2 != null)
+                            _dataUser1 = _dataUser2;
+                        Input = new InputModel
+                        {
+                            Id = _dataUser1.Id,
+                            Name = _dataUser1.Name,
+                            LastName = _dataUser1.LastName,
+                            NID = _dataUser1.NID,
+                            Email = _dataUser1.Email,
+                            Image = _dataUser1.Image,
+                            PhoneNumber = _dataUser1.IdentityUser.PhoneNumber,
+                            rolesLista = getRoles(_dataUser1.Role),
+                        };
+                        if (_dataInput != null)
+                        {
+                            Input.ErrorMessage = _dataInput.ErrorMessage;
+                        }
+                    }
+                }
+            }
             else
             {
                 Input = new InputModel
@@ -62,23 +87,9 @@ namespace SalesSystem.Areas.Users.Pages.Account
                     rolesLista = _usersRole.getRoles(_roleManager)
                 };
             }
-            if(_dataUser1 != null)
-            {
-                Input = new InputModel
-                {
-                    Name = _dataUser1.Name,
-                    LastName = _dataUser1.LastName,
-                    NID = _dataUser1.NID,
-                    Email = _dataUser1.Email,
-                    Image = _dataUser1.Image,
-                    PhoneNumber = _dataUser1.PhoneNumber,
-                    rolesLista = getRoles(_dataUser1.Role),
-                };
-                
-            }
+
             _dataUser2 = _dataUser1;
             _dataUser1 = null;
-           
         }
         [BindProperty]
         public InputModel Input { get; set; }
@@ -87,52 +98,56 @@ namespace SalesSystem.Areas.Users.Pages.Account
             public IFormFile AvatarImage { get; set; }
             [TempData]
             public string ErrorMessage { get; set; }
-
             public List<SelectListItem> rolesLista { get; set; }
         }
-        //metodo por el que registras la informacion del usuario por Registro.cshtml con el post
-
         public async Task<IActionResult> OnPost(String dataUser)
         {
             if (dataUser == null)
             {
-                // se crea a condicion,ejecuta la tarea de registrar los usuarios, si me retorna verdadero
-                //el usuario se HA registrado y esta en las tablas de LA db, ademas nos redirecciona a el siguiente link
-                if (await SaveAsync())
+                if (_dataUser2 == null)
                 {
-                    return Redirect("/Users/Users?area=Users");
+                    if (await SaveAsync())
+                    {
+                        return Redirect("/Users/Users?area=Users");
+                    }
+                    else
+                    {
+                        return Redirect("/Users/Register");
+                    }
                 }
                 else
                 {
-                    return Redirect("/Users/Register");
+                    if (await UpdateAsync())
+                    {
+                        var url = $"/Users/Account/Details?id={_dataUser2.Id}";
+                        _dataUser2 = null;
+                        return Redirect(url);
+                    }
+                    else
+                    {
+                        return Redirect("/Users/Register");
+                    }
                 }
-
+                
             }
             else
             {
-                //obtiene los datos de un usuario y los muestra en el registro para actualizar datos
                 _dataUser1 = JsonConvert.DeserializeObject<InputModelRegister>(dataUser);
-                return Redirect("/Users/Register");
+                return Redirect("/Users/Register?id=1");
             }
 
-            
         }
-        // este metodo lo que hace es salvar los datos en las tablas
-
-        public async Task<bool> SaveAsync()
+        private async Task<bool> SaveAsync()
         {
             _dataInput = Input;
             var valor = false;
-            //esta condicional lo que hace es con el ModelState es capturar la informacion enviada x el post
-            //si la infomacion que metemos es correcta (true) se guarda sino no me deja seguir
             if (ModelState.IsValid)
             {
                 var userList = _userManager.Users.Where(u => u.Email.Equals(Input.Email)).ToList();
                 if (userList.Count.Equals(0))
                 {
                     var strategy = _context.Database.CreateExecutionStrategy();
-                    await strategy.ExecuteAsync(async () =>
-                    {
+                    await strategy.ExecuteAsync(async () => {
                         using (var transaction = _context.Database.BeginTransaction())
                         {
                             try
@@ -146,21 +161,18 @@ namespace SalesSystem.Areas.Users.Pages.Account
                                 var result = await _userManager.CreateAsync(user, Input.Password);
                                 if (result.Succeeded)
                                 {
-                                    //pone el role al usuari que se le mete a la tabla
                                     await _userManager.AddToRoleAsync(user, Input.Role);
-                                    //meto en una variable la comprobacion de de los datos en la tabla 
-                                    //del email,si existe o no, comprobandolo desde el ultimo
                                     var dataUser = _userManager.Users.Where(u => u.Email.Equals(Input.Email)).ToList().Last();
-                                    var imageByte = await _uploadimage.ByteAvatarImageAsync(Input.AvatarImage, _environment,"images/images/default.png");
-                                    var t_user = new TUsers 
-                                    { 
+                                    var imageByte = await _uploadimage.ByteAvatarImageAsync(
+                                        Input.AvatarImage, _environment, "images/images/default.png");
+                                    var t_user = new TUsers
+                                    {
                                         Name = Input.Name,
                                         LastName = Input.LastName,
                                         NID = Input.NID,
                                         Email = Input.Email,
                                         IdUser = dataUser.Id,
                                         Image = imageByte,
-                                    
                                     };
                                     await _context.AddAsync(t_user);
                                     _context.SaveChanges();
@@ -178,7 +190,6 @@ namespace SalesSystem.Areas.Users.Pages.Account
                                     valor = false;
                                     transaction.Rollback();
                                 }
-
                             }
                             catch (Exception ex)
                             {
@@ -187,15 +198,15 @@ namespace SalesSystem.Areas.Users.Pages.Account
                                 valor = false;
                             }
                         }
-
                     });
-                } else
+                }
+                else
                 {
                     _dataInput.ErrorMessage = $"El {Input.Email} ya esta registrado";
                     valor = false;
                 }
-
-            } else
+            }
+            else
             {
                 foreach (var modelState in ModelState.Values)
                 {
@@ -204,13 +215,11 @@ namespace SalesSystem.Areas.Users.Pages.Account
                         _dataInput.ErrorMessage += error.ErrorMessage;
                     }
                 }
-               
                 valor = false;
             }
 
             return valor;
         }
-
         private List<SelectListItem> getRoles(String role)
         {
             List<SelectListItem> rolesLista = new List<SelectListItem>();
@@ -219,8 +228,7 @@ namespace SalesSystem.Areas.Users.Pages.Account
                 Text = role
             });
             var roles = _usersRole.getRoles(_roleManager);
-            roles.ForEach(item =>
-            {
+            roles.ForEach(item => {
                 if (item.Text != role)
                 {
                     rolesLista.Add(new SelectListItem
@@ -231,14 +239,12 @@ namespace SalesSystem.Areas.Users.Pages.Account
             });
             return rolesLista;
         }
-
-        private async Task<bool> UpdateAsybc()
+        private async Task<bool> UpdateAsync()
         {
             var valor = false;
             byte[] imageByte = null;
             var strategy = _context.Database.CreateExecutionStrategy();
-            await strategy.ExecuteAsync(async () =>
-            {
+            await strategy.ExecuteAsync(async () => {
                 using (var transaction = _context.Database.BeginTransaction())
                 {
                     try
@@ -253,15 +259,37 @@ namespace SalesSystem.Areas.Users.Pages.Account
                         if (Input.AvatarImage == null)
                         {
                             imageByte = _dataUser2.Image;
-                        } 
+                        }
                         else
                         {
                             imageByte = await _uploadimage.ByteAvatarImageAsync(Input.AvatarImage, _environment, "");
                         }
+                        var t_user = new TUsers
+                        {
+                            ID = _dataUser2.Id,
+                            Name = Input.Name,
+                            LastName = Input.LastName,
+                            NID = Input.NID,
+                            Email = Input.Email,
+                            IdUser = _dataUser2.ID,
+                            Image = imageByte,
+                        };
+                        _context.Update(t_user);
+                        _context.SaveChanges();
+                        if (_dataUser2.Role != Input.Role)
+                        {
+                            await _userManager.RemoveFromRoleAsync(identityUser, _dataUser2.Role);
+                            await _userManager.AddToRoleAsync(identityUser, Input.Role);
+                        }
+                        transaction.Commit();
+                        valor = true;
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
-                        throw;
+                        _dataInput.ErrorMessage = ex.Message;
+                        transaction.Rollback();
+                        valor = false;
+                        
                     }
                 }
             });
